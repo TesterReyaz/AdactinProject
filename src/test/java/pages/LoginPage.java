8c1e55c1e9f41991caa3be49f01ae70678ab46ac
Govardhan Reddy <100775001+GovardhanReddy8@users.noreply.github.com>
2022-04-26 07:43:19 +0530

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,19 @@
 package pages;
 
 public class LoginPage {
+  WebDriver driver;
+	
+	public void homePage() {
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://adactinhotelapp.com/");
+	}
+
+	public void doLogin(String path, String value) {
+		driver.findElement(By.xpath(path)).sendKeys(value);
+		driver.findElement(By.xpath(path)).click();
+		
+		
+	}
 
 }
